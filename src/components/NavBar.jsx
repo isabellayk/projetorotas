220c826bdc6bdaa4c41304ed65a51f3263c1b893
isabella <isabellay13@gmail.com>
2026-09-10 11:30:36 -0300

    feat: codificando os componentes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,22 @@
+import {Link} from 'react-router-dom'
+
 
 const NavBar = () => {
   return (
-    <>
-      
-    </>
+    <nav className='bg-red-950 shadow-md text-white'>
+
+      <div>
+        <span className='text-xl font-bold text-white'>Projeto</span>
+      </div>
+        <div className='flex space-x4'>
+        <Link to="/" className="flex items-center px-3 py-2 font-medium text-amber-200 hover:text-amber-600">Home</Link>
+        <Link to="/sobre" className="flex items-center px-3 py-2 font-medium text-amber-200 hover:text-amber-600">Sobre</Link>
+        <Link to="/porduto" className="flex items-center px-3 py-2 font-medium text-amber-200 hover:text-amber-600">Produto</Link>
+        <Link to="/contato" className="flex items-center px-3 py-2 font-medium text-amber-200 hover:text-amber-600">Contato</Link>
+        </div>
+
+
+    </nav>
   )
 }
 
